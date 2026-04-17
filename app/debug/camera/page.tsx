3d85313b572922473d7795eeaefbd8030ea9c5d5
折戸哲也 <oritotetsuya@oritotetsuyanoMacBook-Pro.local>
2026-04-18 01:39:10 +0900

    remove duplicate bottom buttons in debug camera

--- app/debug/camera/page.tsx
+++ app/debug/camera/page.tsx
@@ -1292,13 +1292,6 @@ export default function DebugCameraPage() {
             </div>
           </div>
 
-          <div className="shrink-0 bg-black px-5 pt-3 pb-8">
-            <div className="flex items-center justify-between">
-              {PhotoButton}
-              {ShutterButton}
-              {SettingsButton}
-            </div>
-          </div>
         </>
       )}
     </main>
